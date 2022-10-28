8b954212a67c4845f8de65971d5fa3df18e3fe4e
Roberto Portaluri <roberto.portaluri@dovevivoguest.com>
2022-10-28 16:56:27 +0200

    feat(general): added path pageView

--- pages/casa/index.tsx
+++
@@ -1,9 +0,0 @@
-const Ciccio = () => {
-  return (
-    <div className="flex flex-col items-center bg-#cd2737">
-      <h1>Gabry</h1>
-    </div>
-  );
-};
-
-export default Ciccio;
--- pages/casa/next.tsx
+++
@@ -1,9 +0,0 @@
-const Next = () => {
-  return (
-    <div className="flex flex-col items-center bg-#cd2737">
-      <h1>Next</h1>
-    </div>
-  );
-};
-
-export default Next;

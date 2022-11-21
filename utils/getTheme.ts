9d59b399ede87aca375e57e8c8c5b13f689cb7b6
Gabriele Polizzotto <85617777+gabrielepolizzotto-bitrocketdev@users.noreply.github.com>
2022-11-21 11:32:58 +0100

    fix(general): utility get theme can return null now (#50)

--- utils/getTheme.ts
+++ utils/getTheme.ts
@@ -1,7 +1,6 @@
 export const utilityGetTheme = () => {
   if (typeof window !== "undefined") {
-    const theme =
-      localStorage.getItem("startuppario-bitrocket-theme") || "light";
-    return JSON.parse(theme);
+    const theme = localStorage.getItem("startuppario-bitrocket-theme");
+    return theme ? JSON.parse(theme) : null;
   }
 };
